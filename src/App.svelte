b1583635533d10cf1e4633e023e577091e8339bf
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-11-30 13:18:23 -0500

    inverse trig and make the operations look fancier

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,22 @@
 <script>
 	import Tex from './Tex.svelte';
 	import 'katex/dist/katex.min.css';
+	import * as mathjs from 'mathjs';
 	import Button from './Button.svelte';
 	import Keypad from './Keypad.svelte';
 	import evaluatex from "evaluatex/dist/evaluatex";
 	window.evaluatex = evaluatex;
 	let texInputs = [];
 	let score = 0;
+	let question;
+	let answer;
 	function evaluateTexString(texString) {
-		let newTexString = texString.replaceAll("\\pi", "PI");
+		let newTexString = texString.replaceAll("\\pi", "PI").replaceAll(/([0-9])\\/g, "$1*\\");
 		return evaluatex(newTexString, {}, { latex: true })()
 	}
 	$: {
 		try {
-			console.log(evaluateTexString(question), evaluateTexString(texInputs.join("")));
+			console.log(answer, evaluateTexString(texInputs.join("")));
 		} catch (error) {
 			console.log(error);
 		}
@@ -31,23 +34,40 @@
 	function generateQuestion(){
 		const regularTrig = ['sin', 'cos', 'tan', 'sec', 'csc', 'cot']
 		// const inverseTrig = ['asin', 'acos', 'atan', 'asec', 'acsc', 'acot']
-		if (Math.random() > 0) {
+		const inverseTrigPossibleQuestions = {
+			'sin': ["1/2", "\\sqrt2/2", "\\sqrt3/2", "-1/2", "-\\sqrt2/2", "-\\sqrt3/2", "0", "1", "-1"],
+			'csc': ["2", "\\sqrt2", "2\\sqrt3/3", "-2", "-\\sqrt2", "-2\\sqrt3/3", "0", "1", "-1"],
+			'cos': ["1/2", "\\sqrt2/2", "\\sqrt3/2", "-1/2", "-\\sqrt2/2", "-\\sqrt3/2", "0", "1", "-1"],
+			'sec': ["2", "\\sqrt2", "2\\sqrt3/3", "-2", "-\\sqrt2", "-2\\sqrt3/3", "0", "1", "-1"],
+			'tan': ["0", "\\sqrt3/3", "1", "\\sqrt3", "-\\sqrt3/3", "-1", "-\\sqrt3"],
+			'cot': ["0", "\\sqrt3", "1", "\\sqrt3/3", "-\\sqrt3", "-1", "-\\sqrt3/3"]
+		}
+		if (Math.random() > 0.5) {
 			let denominator = [1, 4, 6][Math.floor(Math.random() * 3)];
 			let numerator = Math.floor(Math.random() * 6 * denominator) + 1;
 			[numerator, denominator] = reduce(numerator, denominator);
 			const operation = regularTrig[Math.floor(Math.random() * 6)];
 			if(numerator == 1) numerator = "";
 			let neg = Math.random() > 0.5 ? "-" : "";
+			let q;
 			if(denominator == 1){
-				return `${operation}(${neg}${numerator}\\pi)`
+				q = `\\${operation}(${neg}${numerator}\\pi)`
 			} else {
-				return `${operation}(${neg}${numerator}\\pi/${denominator})`
+				q = `\\${operation}(${neg}${numerator}\\pi/${denominator})`
 			}
-		} // else do the inverse trig
+			let a = evaluateTexString(q);
+			question = q;
+			answer = a;
+		} else {
+			let operation = regularTrig[Math.floor(Math.random() * 6)];
+			let eqation = inverseTrigPossibleQuestions[operation][Math.floor(Math.random() * inverseTrigPossibleQuestions[operation].length)];
+			question = `\\${operation}^{-1}(${eqation})`;
+			answer = mathjs["a"+operation](evaluateTexString(eqation));
+		}
 	}
-	let question = generateQuestion();
+	generateQuestion();
 	function submitAnswer(){
-		let q = evaluateTexString(question)
+		let q = answer
 		if(Math.abs(q) > 1000000 && texInputs.length == 0){
 			score++;
 		} else {
@@ -63,7 +83,7 @@
 				score = 0;
 			}
 		}
-		question = generateQuestion();
+		generateQuestion();
 		texInputs = [];
 	}
 </script>
